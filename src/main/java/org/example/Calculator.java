0593a5325888c03c154ba1677e605c8c8463dd65
Rodrigo Lizama <rlizama@facele.cl>
2025-09-16 05:55:32 -0300

    commit2

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,9 +5,18 @@ public class Calculator {
         return a + b;
     }
 
-
-    public int divide(int a, int b) {
-        return a / b;
+    public int subtract(int a, int b) {
+        return a - b;
     }
 
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,17 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     Calculator calc = new Calculator();
+
     @Test
     void testAddition() {
-        assertEquals(5,calc.add(2,3));
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(1, calc.subtract(3, 2));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(6, calc.multiply(2, 3));
     }
 
     @Test
     void testDivision() {
-        assertEquals(2,calc.divide(10,5));
+        assertEquals(2, calc.divide(10, 5));
     }
+
     @Test
-    void testDivisionbyZero() {
-        assertThrows(ArithmeticException.class, () -> calc.divide(10,0));
+    void testDivisionByZero() {
+        assertThrows(ArithmeticException.class, () -> calc.divide(10, 0));
     }
 }
